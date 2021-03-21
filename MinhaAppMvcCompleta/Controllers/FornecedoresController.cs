5e9e51a960f0c7cf567caa0673b60901dc348927
Ariovaldo Barbosa Da Silva Junior <ariovaldo.junior@ltm.digital>
2021-03-21 04:13:23 -0300

    Create FornecedorController

---
+++ MinhaAppMvcCompleta/Controllers/FornecedoresController.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using DevIO.App.ViewModels;
+using DevIO.Business.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using MinhaAppMvcCompleta.Data;
+using System.Threading.Tasks;
+
+namespace DevIO.App.Controllers
+{
+    public class FornecedoresController : Controller
+    {
+        private readonly IFornecedorRepository _fornecedorRepository;
+        private readonly IMapper _mapper;
+
+        public FornecedoresController(IFornecedorRepository fornecedorRepository, IMapper mapper)
+        {
+            _fornecedorRepository = fornecedorRepository;
+            _mapper = mapper;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(_mapper.Map<FornecedorViewModel>(await _fornecedorRepository.ObterTodos()));
+        }
+    }
+}
